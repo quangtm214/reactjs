8ba33e92cdb65d897710c0de0f3ed7ef42a6b17f
laptop-m8du3f9f <quangtran214203@gmail.com>
2023-09-21 16:51:25 +0700

    Accordion

---
+++ jquery/index.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $("#home, #aboutus").click(function(){
+     alert("home");
+    });
+
+    // $("#aboutus").click(function(){
+    //     alert("aboutus");
+    //    });
+
+    //    $("#menu221").click(function(){
+    //     alert("menu");
+    //    });
+
+    //    $("#contact221").click(function(){
+    //     alert("contact");
+    //    });
+  });
